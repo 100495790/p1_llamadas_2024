bffdcb8538137142ad31ba653aedebe855b2716f
Lucas <ldelvillarvazquez@gmail.com>
2024-02-24 22:41:36 +0100

    myls

--- myls.c
+++ myls.c
@@ -7,10 +7,32 @@
 #include <string.h>
 
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+	char *dirname;
+
+	/* Si no no especifican ningún directorio, trabajamos sobre el actual */
+	if (argc < 2) {
+		char dir_actual[1024];
+		dirname = getcwd(dir_actual, sizeof(dir_actual));
+	} else {
+		dirname = argv[1];
+	}
+	
+	DIR *dirp = opendir(dirname);	// Abrimos el directorio
+
+	if (&dirp == NULL) {
+		perror("Error al abrir el directorio\n");
+		return -1;
+	}
+	
+	struct dirent *entry;
+	while ((entry = readdir(dirp)) != NULL) {
+        // Imprime el nombre de cada entrada
+        printf("%s\n", entry->d_name);
+    }
 	
 
+	closedir(dirp);	// Cerramos el directorio
 	return 0;
 }
 
